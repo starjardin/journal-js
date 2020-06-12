9cfe8fd361590e38b74295e6027e29466da4e473
Tantely Andrianarivola <tantely.and@onja.org>
2020-06-12 07:32:54 +0300

    created a prompt - starting menu

--- journal.js
+++ journal.js
@@ -1,1 +1,8 @@
+// use object to crate smt
+// create a prompt for the user
+// array contains the journal entries
+// while loop to ask the user until they choose quit
+// if prompt === 1 display already exist entry
+// if prompt === 2 here where we entered the new journal.
 
+let startMenu = prompt('Welcome to Onja journal! \n Choose(1) for listing all the entires \n Choose (2) for adding a new entry \n Choose (3) to quit')
